4d2b1c5f88cfca4e45e5eb1e20a593ad67bce48e
Matthew Cibulka <mcibulkadev@gmail.com>
2018-08-29 19:43:58 -0400

    Removed React template code and logo.svg.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Header from './components/Header.js'
 import './App.css';
+import Header from './components/Header.js'
 import Instructions from './components/Instructions';
 
 class App extends Component {
@@ -10,13 +9,6 @@ class App extends Component {
       <div className="App">
         <Header />
         <Instructions />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
