0c43c10f34c88ca69befceb913d845f808d38bab
Matthew Cibulka <mcibulkadev@gmail.com>
2018-08-29 19:39:09 -0400

    Added Instructions component.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Header from './components/Header.js'
 import './App.css';
+import Instructions from './components/Instructions';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Instructions />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
---
+++ src/components/Instructions.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Instructions = () => (
+  <div className="columns is-centered">
+    <div className="column has-text-centered">
+      <div>
+        <p className="title is-2">Clicky Game!</p>
+        <p className="subtitle is-3">Click on an image to earn points, but don't click on any more than once!</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default Instructions;
