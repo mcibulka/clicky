b15b52fb9aaaf8ff6880f9e6a962290878ead19c
Matthew Cibulka <mcibulkadev@gmail.com>
2018-08-31 09:28:10 -0400

    Added Square component and refactored GameBoard to include Square.

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,72 +1,48 @@
 import React from "react";
-import bmo from "../images/BMO.png"
+import Square from "./Square";
 
 const GameBoard = () => (
   <div>
     <div className="columns is-centered">
       <div className="column is-2">
-        <figure className="image is-square">
-          <img src={bmo} alt=""></img>
-        </figure>
+        <Square />
       </div>
       <div className="column is-2">
-        <figure className="image is-square">
-          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
-        </figure>
+        <Square />
       </div>
       <div className="column is-2">
-        <figure className="image is-square">
-          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
-        </figure>
+        <Square />
       </div>
       <div className="column is-2">
-        <figure className="image is-square">
-          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
-        </figure>
+        <Square />
       </div>
     </div>
     <div className="columns is-centered">
       <div className="column is-2">
-        <figure className="image is-square">
-          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
-        </figure>
+        <Square />
       </div>
       <div className="column is-2">
-        <figure className="image is-square">
-          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
-        </figure>
+        <Square />
       </div>
       <div className="column is-2">
-        <figure className="image is-square">
-          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
-        </figure>
+        <Square />
       </div>
       <div className="column is-2">
-        <figure className="image is-square">
-          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
-        </figure>
+        <Square />
       </div>
     </div>
     <div className="columns is-centered">
       <div className="column is-2">
-        <figure className="image is-square">
-          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
-        </figure>
+        <Square />
       </div>
       <div className="column is-2">
-        <figure className="image is-square">
-          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
-        </figure>
+        <Square />
       </div>
       <div className="column is-2">
-        <figure className="image is-square">
-          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
-        </figure>
+        <Square />
       </div>
       <div className="column is-2">
-        <figure className="image is-square">
-          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
-        </figure>
+        <Square />
       </div>
     </div>
   </div>
---
+++ src/components/Square.js
@@ -0,0 +1,12 @@
+import React from "react";
+import bmo from "../images/BMO.png"
+
+const Square = () => (
+  <div>
+    <figure className="image is-square">
+      <img src={bmo} alt=""></img>
+    </figure>
+  </div>
+);
+
+export default Square;
