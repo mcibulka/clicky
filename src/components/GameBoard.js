febff65eb531ffe9114efd5e40071b798be3f401
Matthew Cibulka <mcibulkadev@gmail.com>
2018-08-31 09:18:14 -0400

    Updated h1 to img tags + sample image displayed.

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,49 +1,72 @@
 import React from "react";
+import bmo from "../images/BMO.png"
 
 const GameBoard = () => (
   <div>
     <div className="columns is-centered">
       <div className="column is-2">
         <figure className="image is-square">
-          <img src="https://bulma.io/images/placeholders/128x128.png"></img>
+          <img src={bmo} alt=""></img>
         </figure>
       </div>
       <div className="column is-2">
-        <h1>Image 2</h1>
+        <figure className="image is-square">
+          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
+        </figure>
       </div>
       <div className="column is-2">
-        <h1>Image 3</h1>
+        <figure className="image is-square">
+          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
+        </figure>
       </div>
       <div className="column is-2">
-        <h1>Image 4</h1>
-      </div>
-    </div>
-    <div className="columns is-centered">
-      <div className="column is-2">
-        <h1>Image 5</h1>
-      </div>
-      <div className="column is-2">
-        <h1>Image 6</h1>
-      </div>
-      <div className="column is-2">
-        <h1>Image 7</h1>
-      </div>
-      <div className="column is-2">
-        <h1>Image 8</h1>
+        <figure className="image is-square">
+          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
+        </figure>
       </div>
     </div>
     <div className="columns is-centered">
       <div className="column is-2">
-        <h1>Image 9</h1>
+        <figure className="image is-square">
+          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
+        </figure>
       </div>
       <div className="column is-2">
-        <h1>Image 10</h1>
+        <figure className="image is-square">
+          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
+        </figure>
       </div>
       <div className="column is-2">
-        <h1>Image 11</h1>
+        <figure className="image is-square">
+          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
+        </figure>
       </div>
       <div className="column is-2">
-        <h1>Image 12</h1>
+        <figure className="image is-square">
+          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
+        </figure>
+      </div>
+    </div>
+    <div className="columns is-centered">
+      <div className="column is-2">
+        <figure className="image is-square">
+          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
+        </figure>
+      </div>
+      <div className="column is-2">
+        <figure className="image is-square">
+          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
+        </figure>
+      </div>
+      <div className="column is-2">
+        <figure className="image is-square">
+          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
+        </figure>
+      </div>
+      <div className="column is-2">
+        <figure className="image is-square">
+          <img src="https://bulma.io/images/placeholders/96x96.png" alt=""></img>
+        </figure>
       </div>
     </div>
   </div>
