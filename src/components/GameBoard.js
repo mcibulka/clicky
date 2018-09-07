2943ff3fd0a94a148eb07d9c50a575a288465883
Matthew Cibulka <mcibulkadev@gmail.com>
2018-09-06 21:25:19 -0400

    Converted components from functions to classes; ES6.

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,51 +1,55 @@
 import React from "react";
 import Square from "./Square";
 
-const GameBoard = () => (
-  <div>
-    <div className="columns is-centered">
-      <div className="column is-2">
-        <Square />
+class GameBoard extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="columns is-centered">
+          <div className="column is-2">
+            <Square />
+          </div>
+          <div className="column is-2">
+            <Square />
+          </div>
+          <div className="column is-2">
+            <Square />
+          </div>
+          <div className="column is-2">
+            <Square />
+          </div>
+        </div>
+        <div className="columns is-centered">
+          <div className="column is-2">
+            <Square />
+          </div>
+          <div className="column is-2">
+            <Square />
+          </div>
+          <div className="column is-2">
+            <Square />
+          </div>
+          <div className="column is-2">
+            <Square />
+          </div>
+        </div>
+        <div className="columns is-centered">
+          <div className="column is-2">
+            <Square />
+          </div>
+          <div className="column is-2">
+            <Square />
+          </div>
+          <div className="column is-2">
+            <Square />
+          </div>
+          <div className="column is-2">
+            <Square />
+          </div>
+        </div>
       </div>
-      <div className="column is-2">
-        <Square />
-      </div>
-      <div className="column is-2">
-        <Square />
-      </div>
-      <div className="column is-2">
-        <Square />
-      </div>
-    </div>
-    <div className="columns is-centered">
-      <div className="column is-2">
-        <Square />
-      </div>
-      <div className="column is-2">
-        <Square />
-      </div>
-      <div className="column is-2">
-        <Square />
-      </div>
-      <div className="column is-2">
-        <Square />
-      </div>
-    </div>
-    <div className="columns is-centered">
-      <div className="column is-2">
-        <Square />
-      </div>
-      <div className="column is-2">
-        <Square />
-      </div>
-      <div className="column is-2">
-        <Square />
-      </div>
-      <div className="column is-2">
-        <Square />
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default GameBoard;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,16 @@
 import React from "react";
 import bmo from "../images/BMO.png"
 
-const Square = () => (
-  <div>
-    <figure className="image is-square">
-      <img src={bmo} alt=""></img>
-    </figure>
-  </div>
-);
+class Square extends React.Component {
+  render() {
+    return (
+      <div>
+        <figure className="image is-square">
+          <img src={bmo} alt=""></img>
+        </figure>
+      </div>
+    );
+  }
+}
 
 export default Square;
